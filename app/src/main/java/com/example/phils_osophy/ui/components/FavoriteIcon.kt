99c5d42161be10ecb99c55bf1c4d9b134c3f1c4f
s4ndr0sch3rr3r <72465901+s4ndr0sch3rr3r@users.noreply.github.com>
2026-07-14 19:50:44 +0200

    Fix favorite icon size import

--- app/src/main/java/com/example/phils_osophy/ui/components/FavoriteIcon.kt
+++ app/src/main/java/com/example/phils_osophy/ui/components/FavoriteIcon.kt
@@ -1,5 +1,6 @@
 package com.example.phils_osophy.ui.components
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
